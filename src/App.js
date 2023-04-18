4ffaf1d5590a015c971d4a19d049c15f7bf8928f
Faris Khalid <fariskhalid10@outlook.com>
2023-04-18 19:02:06 +0100

    books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" component={Books} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
