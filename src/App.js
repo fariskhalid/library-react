17b3fd17a25e7b66889792aa5be533a0b0ba6266
Faris Khalid <fariskhalid10@outlook.com>
2023-04-19 22:08:06 +0100

    bookinfo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+
 
 function App() {
   return (
@@ -10,7 +13,8 @@ function App() {
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books" component={Books} />
+      <Route path="/books" exact render={() => <Books books={books} />}/>
+      <Route path="/books/:id" render={() => <BookInfo books={books} />} />
       <Footer />
     </div>
     </Router>
--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Explore = () => {
   return (
     <section id='explore'>
-        <div className="conatiner">
+        <div className="container">
             <div className="row row__column">
                 <h2>
                     Explore More <span className='purple'>Books</span>
                 </h2>
-                <a href="/books">
+                <Link to="/books">
                     <button className='btn'>Explore books</button>
-                </a>
+                </Link>
             </div>
         </div>
     </section>
